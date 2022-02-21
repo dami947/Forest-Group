0eedbf4121163cdd45ddbea9556bb5f049f05476
Damian <tobres.dk@gmail.com>
2022-02-21 19:48:34 +0100

    Forest Group

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+const navBtn = document.querySelector('.nav-btn')
+const navMobile = document.querySelector('.nav-mobile');
+const navAllLink = document.querySelectorAll('.nav-link')
+const footerYear = document.querySelector('.footer-year');
+
+
+
+
+
+
+const activeNav = () => {
+    document.body.classList.toggle('sticky-body')
+    navMobile.classList.toggle('nav-mobile-active')
+}
+
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+
+navAllLink.forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.classList.remove('sticky-body')
+        navMobile.classList.remove('nav-mobile-active')
+    })
+})
+
+
+
+handleCurrentYear();
+
+
+navBtn.addEventListener('click', activeNav);
